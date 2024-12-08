2ce93946c0bd50ed5e686bb7c7ebbe3ce52148b4
3alifn <3alifn30@gmail.com>
2024-12-08 12:04:25 +0600

    terminal lics system bug fix

--- app.js
+++ app.js
@@ -20,21 +20,30 @@ app.all('*', (req, res, next) => {
   const hostnameInt = host ? req.hostname.split("www.")[1] : req.hostname;
   res.cookie('hostname', hostnameInt, { path: "/", sameSite: "strict", httpOnly: true, priority: "high" });
 
+ if(req.url=="/ini/lics/checkout/"){
+  next()
+ }
+  
+ else {
   sqlmap.query(
-      `SELECT domain, lics FROM ___ini WHERE domain=? AND at_status=? AND checkout=?`,
-      [hostnameInt, true, true],
-      (errllc, infollc) => {
-          if (errllc) {
-              console.log(errllc.sqlMessage);
-              return;
-          }
-          if (infollc.length > 0) {
-              next();
-          } else {
-              res.render('ini/lics');
-          }
-      }
-  );
+    `SELECT domain, lics FROM ___ini WHERE domain=? AND at_status=? AND checkout=?`,
+    [hostnameInt, true, true],
+    (errllc, infollc) => {
+        if (errllc) {
+            console.log(errllc.sqlMessage);
+            return;
+        }
+        if (infollc.length > 0) {
+            next();
+        } else {
+            res.render('ini/lics');
+        }
+    }
+);
+ }
+
+
+
 });
 
 
